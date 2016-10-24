92c238c473b039cd0d7665a2f79658fb39c48669
Gerard de Jong <gerard@dejong.za.net>
2016-10-24 10:42:33 +0200

    Moving code over to it's own repository for further development

---
+++ Schema Validator/Schema Validator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Schema Validator
+//
+//  Created by Gerard de Jong on 2016/10/24.
+//  Copyright © 2016 Gerard de Jong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
